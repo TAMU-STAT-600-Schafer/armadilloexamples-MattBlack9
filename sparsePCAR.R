b3b468e95e873b3db3110c7e5e9df8ab3063c240
Matthew Blackley <123616859+MattBlack9@users.noreply.github.com>
2024-10-17 10:02:26 -0500

    Create sparsePCAR.R

---
+++ sparsePCAR.R
@@ -0,0 +1,25 @@
+procrustesR <- function(X, V){
+svdXV <- svd(X %*% V)
+U <- tcrossprod(svdXV$u, svdXV$v)
+return(U)
+}
+
+sparsePCAR <- function(X, Vstart, lambda, tol){
+  U <- procrustesR(X, Vstart)
+
+  V <- Vstart
+
+  old_obj <- sum((X - tcrossprod(U, V))^2) / 2 + lambda + sum(abs(V))
+
+  error <- 100
+
+  while(error > tol){
+    XtU <- crossprod(X,U)
+    V <- sign(XtU) * pmax(abs(XtU) - lambda, 0)
+    U <- procrustesR(X, V)
+    new_obj <- sum((X - tcrossprod(U, V))^2) / 2 + lambda + sum(abs(V))
+    error <- abs(new_obj - old_obj)
+    old_obj <- new_obj
+    }
+  
+  }
